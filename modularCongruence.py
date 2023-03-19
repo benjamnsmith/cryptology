dc09c1118cb42620486114cd82653c176ec0f125
Ben Smith <benjaminwrobelsmith@gmail.com>
2023-03-19 17:48:50 -0500

    mod congruence tmp

--- modularCongruence.py
+++ modularCongruence.py
@@ -1,3 +1,8 @@
+"""
+My scratch doc to brute force check congruence
+"""
+
+
 def checkCongruence():
     total = {}
     for mod in range(2,4):
@@ -13,5 +18,15 @@ def checkCongruence():
                         total[mod] += 1
 
         print("(mod {}): {}".format(mod, total[mod]/mod**3))
+
+
+def homework6_congruences():
+    for x in range(15):
+        for y in range(15):
+            exp1 = (9 * x + 12 * y) % 21 == 3
+            exp2 = (11 * x + 5 * y) % 21 == 12
+            if exp1 and exp2:
+                print("Found ({},{})".format(x, y))
         
+homework6_congruences()
     
